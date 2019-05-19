47daf88ec9bb118585738c299dd8c09698c26ec6
Artem30801 <artem30801@gmail.com>
2019-05-19 15:39:53 +0300

    Prettify

--- app.py
+++ app.py
@@ -3,41 +3,42 @@ from sanic import Sanic
 from sanic import response
 
 app = Sanic()
-count = len(os.listdir(os.path.abspath('frames/')))
+frames_path = 'frames/'
+count = len(os.listdir(os.path.abspath(frames_path)))
 
 
 @app.route("/")
 async def index(request):
-    return response.redirect('/test')
+    return response.redirect('/read')
 
 
-@app.route("/test")
-async def test(request):
+@app.route("/read")
+async def reader_page(request):
     return response.html(open(os.path.abspath('test/skybox_markup.html'), encoding='utf-8').read())
 
 
 @app.route("/style.css")
-async def test(request):
+async def style(request):
     return await response.file(os.path.abspath('test/style.css'))
 
 
 @app.route("/script.js")
-async def test(request):
+async def script(request):
     return await response.file(os.path.abspath('test/script.js'))
 
 
 @app.route("/favicon.png")
-async def test(request):
+async def favicon(request):
     return await response.file(os.path.abspath('test/favicon.png'))
 
 
 @app.route('/frames/<num:int>.jpg')
-async def post_handler(request, num):
+async def get_frame(request, num):
     if num >= count - 1:
         num = count - 1
     elif num <= 0:
         num = 0
-    path = os.path.abspath('frames/{}.jpg'.format(str(num)))
+    path = os.path.abspath(frames_path+'{}.jpg'.format(str(num)))
     return await response.file(path)
 
 
